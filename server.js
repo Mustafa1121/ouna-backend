c395418ae82b61c923df9926cf0749369b7fbf24
Mustafa khodor <87423616+Mustafa1121@users.noreply.github.com>
2023-05-09 23:15:00 +0300

    Address system && Product System

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(require("body-parser").urlencoded({ extended: false, limit: "50mb" }));
 app.use(require("cors")());
 
 // Routes
+app.use('/api/user/auth',require('./routes/User/UserAuthRoute'))
+app.use('/api/user/address',require('./routes/Address/AddressRoute'))
+app.use('/api/products',require('./routes/Product/ProductRoute'))
 
 const PORT = process.env.PORT || 3306;
 app.listen(PORT, console.log(`server is running at http://localhost:${PORT}`));
