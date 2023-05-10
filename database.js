f851474f4fbcb62d10cf5b2914b1d847b0dc255f
Mustafa khodor <87423616+Mustafa1121@users.noreply.github.com>
2023-05-10 22:34:03 +0300

    vercel deploy

--- database.js
+++ database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 exports.connectDB = async () => {
     try {
+        console.log(process.env.MONGO_URL)
         await mongoose.connect(process.env.MONGO_URL)
         console.log('mongoDB connected')
     } catch (error) {
